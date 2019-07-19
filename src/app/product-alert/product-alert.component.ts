794257eea50a46dadc78f20912867fc96fbbaefa
qazisohail101 <qazisohail101@gmail.com>
2019-07-19 14:10:21 +0200

    4 files modified

--- src/app/product-alert/product-alert.component.ts
+++ src/app/product-alert/product-alert.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
-
+import { Output, EventEmitter} from '@angular/core';
 @Component({
   selector: 'app-product-alert',
   templateUrl: './product-alert.component.html',
@@ -8,14 +8,12 @@ import { Input } from '@angular/core';
 })
 export class ProductAlertComponent implements OnInit {
   @Input() product;
+  @Output() notify=new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
     
-  }
-
-  notifyMe(){
-    window.alert("You will be notified about it");
   }
 
 }
