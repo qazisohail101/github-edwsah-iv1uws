d14a61f9669001850cbed4361a49a2a5975266e6
qazisohail101 <qazisohail101@gmail.com>
2019-07-19 13:30:33 +0200

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -14,8 +14,10 @@ export class ProductDetailsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
+    //subscribe to the parameters received which are received in the arrow function variable params..then in the expression of arrow variable, we get the value of the productId parameter, put it in the index of the products array, and assign the object to our product property. simple
     this.route.paramMap.subscribe(
-      params => {this.product= products[];}
+      params => {this.product= products[params.get('productId')];}
     );
   }
 
